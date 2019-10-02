49e564aeaf7ec5392fac360c79b10119e6e914b4
Sven Fuchs <me@svenfuchs.com>
2019-10-02 20:27:46 +0200

    Update foo.rb

--- foo.rb
+++ foo.rb
@@ -1,2 +1,2 @@
-p system("git config user.email >/dev/null 2>/dev/null || git config user.email `whoami`@localhost")
+p system("git config user.email >/dev/null 2>/dev/null || git config user.email $(whoami)@localhost")
 puts `git config user.email`
